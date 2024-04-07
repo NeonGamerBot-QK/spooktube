7b31293327c7d4cdd7a96832508b10e8020b5bce
Saahil <neon@saahild.com>
2024-04-07 00:05:58 -0400

    v1

--- src/index.js
+++ src/index.js
@@ -9,10 +9,13 @@ const { exec } = require('child_process')
 const ejs = require('ejs')
 // disable express
 app.disable('x-powered-by')
-
-app.use(express.static('public'))
+const includeFile = (filePath, ops = { include: includeFile}) => {
+  return ejs.render(fs.readFileSync(path.join(__dirname, 'views', filePath)).toString(), ops)
+}
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.engine('ejs', (filePath, options, callback) => { // define the template engine
+  options = { ...options, include: includeFile }
   fs.readFile(filePath, (err, content) => {
     if (err) return callback(err)
       // this is an extremely simple template engine
@@ -25,7 +28,18 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   res.render('index')
 })
-
+app.get('/upload', (req, res) => {
+  res.render('upload')
+})
+app.get('/video/:id', (req, res) => {
+  // const db = new Database(path.join(__dirname, 'db', 'db.json'))
+  // const video = db.get(req.params.id)
+  // if (video) {
+    res.render('video', { video })
+  // } else {
+  //   res.status(404).send('Video not found')
+  // }
+})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
 })
