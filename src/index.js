ac35cb4600443ca298650677700343c7c3e7acab
Saahil <neon@saahild.com>
2024-04-11 13:40:29 -0400

    partial

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const { exec } = require('child_process')
 const ejs = require('ejs')
 // disable express
 app.disable('x-powered-by')
+const db = new Database(path.join(__dirname, 'db', 'db.json'))
 const includeFile = (filePath, ops = { include: includeFile}) => {
   return ejs.render(fs.readFileSync(path.join(__dirname, 'views', filePath)).toString(), ops)
 }
@@ -35,15 +36,117 @@ app.get('/video/:id', (req, res) => {
   // const db = new Database(path.join(__dirname, 'db', 'db.json'))
   // const video = db.get(req.params.id)
   // if (video) {
-  res.render('video', { video })
+  res.render('video')
   // } else {
   //   res.status(404).send('Video not found')
   // }
 })
+app.post('/video/create', (req, res) => {
+  const {
+    name, 
+    description,
+    video
+  } = req.body
+  const id = crypto.randomBytes(16).toString('hex').slice(0,6)
+  db.set(id, {
+    id,
+    name,
+    description,
+    video,
+    likes: 0,
+    dislikes: 0,
+    views: 0,
+    comments: []
+  })
+  res.status(201).json({ id })
+
+})
+app.post('/video/:id/like', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    video.likes++
+    db.set(req.params.id, video)
+    res.status(200).json({ likes: video.likes })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.post('/video/:id/dislike', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    video.dislikes++
+    db.set(req.params.id, video)
+    res.status(200).json({ dislikes: video.dislikes })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.post('/video/:id/view', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    video.views++
+    db.set(req.params.id, video)
+    res.status(200).json({ views: video.views })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.post('/video/:id/comment', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    const { comment } = req.body
+    video.comments.push(comment)
+    db.set(req.params.id, video)
+    res.status(200).json({ comments: video.comments })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.get('/video/:id/comments', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    res.status(200).json({ comments: video.comments })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.get('/video/:id/likes', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    res.status(200).json({ likes: video.likes })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.get('/video/:id/dislikes', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    res.status(200).json({ dislikes: video.dislikes })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
+app.get('/video/:id/views', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    res.status(200).json({ views: video.views })
+  } else {
+    res.status(404).send('Video not found')
+  }
+}
+)
+app.get('/video/:id/data', (req, res) => {
+  const video = db.get(req.params.id)
+  if (video) {
+    res.status(200).json({ video })
+  } else {
+    res.status(404).send('Video not found')
+  }
+})
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`)
 })
- // Automatic 30second git pull.
+ // Automatic 30 second git pull.
 setInterval(() => {
   exec(`git pull`, (error, stdout) => {
     let response = error || stdout
---
+++ src/tests/get_dimensions.js
@@ -0,0 +1,10 @@
+process.env.DEBUG = '*'
+process.env.DEBUG_COLORS = true
+var getDimensions = require('get-video-dimensions');
+const path = require('path')
+const publicPath = path.join(__dirname, '..', 'public')
+getDimensions( publicPath + '/example.webm').then(function (dimensions) {
+  console.log(dimensions.width);
+  console.log(dimensions.height);
+  console.log(dimensions)
+})
